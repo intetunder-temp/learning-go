498d9bd5517bbc8db5d970ff3f4e4e92b0cbe368
intetunder-temp <github-temp@darknet.fyi>
2020-03-21 14:03:23 +0000

    first commit

---
+++ pluralsight/getting-started/hello-world/main.go
@@ -0,0 +1,14 @@
+package main
+
+// This is an import block, it allows multiple packages to be imported without
+// repeating the "import" keyword
+import (
+	"fmt"
+)
+
+/* The main function, when part of the main package, indetifies the entry point
+of an application
+*/
+func main() {
+	fmt.Println("Hello, World")
+}
