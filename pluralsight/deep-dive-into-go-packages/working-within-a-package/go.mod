97229730bb249cce14b281e3c23530b8cdb7ba1f
intetunder-temp <github-temp@darknet.fyi>
2020-03-24 20:57:16 +0000

    initial commit

---
+++ pluralsight/deep-dive-into-go-packages/working-within-a-package/go.mod
@@ -0,0 +1,3 @@
+module github.com/intetunder-temp/learning-go/pluralsight/deep-dive-into-go-packages/working-within-a-package
+
+go 1.14
---
+++ pluralsight/deep-dive-into-go-packages/working-within-a-package/services/events/serve.go
@@ -0,0 +1,33 @@
+/*
+ Package events provides a webservice that manages the libary's special events.
+*/
+package events
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/pluralsight/libmanager/services/internal/ports"
+)
+
+var port = 42
+
+// StartServer registers the handlers and initiates the web service.
+// The service is started on the local machine with the port specified by
+// .../lm/services/internal/ports#EventService
+func StartServer() error {
+	sm := http.NewServeMux()
+	sm.Handle("/", new(eventHandler))
+	return http.ListenAndServe(":"+strconv.Itoa(port), sm)
+}
+
+func init() {
+	fmt.Println("serve.go 1", port)
+	port = ports.EventService
+	fmt.Println("serve.go 2", port)
+}
+
+func init() {
+	fmt.Println("second init in serve.go")
+}
